4930ca62c83d93a00322a2137801b95b46386a88
Abdirazakf <farahabdirazak13@gmail.com>
2025-09-08 20:27:50 -0400

    Completed gameboard module and tested matrix and ship placement

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -3,7 +3,70 @@ export default class Gameboard {
         this.rows = 10
         this.cols = 10
         this.board = Array.from({length: this.rows}, () => 
-            new Array(this.cols).fill(0)
+            new Array(this.cols).fill(null)
         )
+    }
+
+    validPosition(ship, row, col, direction) {
+        if (row < 0 || row >= this.rows || col < 0 || col >= this.cols) {
+            return false
+        }
+        
+        if (direction === 'horizontal') {
+            if (col + ship.length > this.cols) {
+                return false
+            }
+        } else if (direction === 'vertical') {
+            if (row + ship.length > this.rows) {
+                return false
+            }
+        }
+
+        
+        if (direction === 'horizontal') {
+            for (let i = 0; i < ship.length; i++) {
+                if (this.board[row][col + i] !== null) {
+                    return false
+                }
+            }
+        } else if (direction === 'vertical') {
+            for (let i = 0; i < ship.length; i++) {
+                if (this.board[row + i][col] !== null) {
+                    return false
+                }
+            }
+        }
+        
+        return true
+    }
+
+    placeShip(ship, row, col, direction) {
+        if (!this.validPosition(ship, row, col, direction)) {
+            return false
+        }
+
+        if (direction === 'horizontal') {
+            for (let i = 0; i < ship.length; i++) {
+                this.board[row][col + i] = ship
+            }
+        } else if (direction === 'vertical') {
+            for (let i = 0; i < ship.length; i++) {
+                this.board[row + i][col] = ship
+            }
+        }
+
+        return true
+    }
+
+    receiveAttack(row, col) {
+        if (this.board[row][col] === null) {
+            this.board[row][col] = 'miss'
+            return false
+        } else if (this.board[row][col] !== 'miss' || this.board[row][col] !== 'hit') {
+            const ship = this.board[row][col]
+            ship.hit()
+            this.board[row][col] = 'hit'
+            return true
+        }
     }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,2 +1,78 @@
-import Gameboard from "../modules/gameboard.js"
+import Gameboard from '../modules/gameboard'
+import Ship from '../modules/ships'
 
+
+describe('Gameboard tests', () => {
+    let gameboard
+    let destroyer
+    let patrolBoat
+
+    beforeEach(() => {
+        gameboard = new Gameboard()
+        destroyer = new Ship('Destroyer', 3)
+        patrolBoat = new Ship('Patrol Boat', 2)
+    })
+
+    describe('board initialization', () => {
+        test('create 10x10 gameboard', () => {
+            expect(gameboard.board.length).toBe(10)
+            expect(gameboard.board[0].length).toBe(10)
+        })
+
+        test('full of null values', () => {
+            gameboard.board.forEach(row => {
+                row.forEach(cell => {
+                    expect(cell).toBeNull
+                })
+            })
+        })
+    })
+
+    describe('Ship placement', () => {
+        test('place ship horizontal', () => {
+            const result = gameboard.placeShip(
+                destroyer, 0, 0, 'horizontal'
+            )
+
+            expect(result).toBeTruthy()
+            expect(gameboard.board[0][0]).toBe(destroyer)
+            expect(gameboard.board[0][1]).toBe(destroyer)
+            expect(gameboard.board[0][2]).toBe(destroyer)
+        })
+
+        test('place ship vertical', () => {
+            const result = gameboard.placeShip(
+                patrolBoat, 5, 3, 'vertical'
+            )
+
+            expect(result).toBeTruthy()
+            expect(gameboard.board[6][3]).toBe(patrolBoat)
+        })
+
+        test('prevent out of bounds horizontally', () => {
+            const result = gameboard.placeShip(destroyer, 0, 8, 'horizontal')
+
+            expect(result).toBeFalsy()
+        })
+
+        test('prevent out of bounds vertically', () => {
+            const result = gameboard.placeShip(destroyer, 8, 0, 'vertical')
+
+            expect(result).toBeFalsy()
+        })
+
+        test('prevent overlap', () => {
+            gameboard.placeShip(destroyer, 5, 5, 'horizontal')
+            const result = gameboard.placeShip(patrolBoat, 5, 6, 'horizontal')
+
+            expect(result).toBeFalsy()
+        })
+
+        test('adjacent ships', () => {
+            gameboard.placeShip(destroyer, 3, 3, 'horizontal')
+            const result = gameboard.placeShip(patrolBoat, 3, 6, 'vertical')
+
+            expect(result).toBeTruthy()
+        })
+    })
+})
