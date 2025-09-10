887b026e9c20a816e295bde4087b0521b193bb23
Abdirazakf <farahabdirazak13@gmail.com>
2025-09-09 20:26:33 -0400

    Created the initial page design with a form modal to take in user name

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,5 +1,27 @@
 import Player from './player.js'
+import UI from './ui.js'
+
+const ui = new UI
 
 export default class Game {
-    
+    constructor() {
+        this.playerName = null
+    }
+
+    getPlayerName() {
+        ui.elements.form.addEventListener('submit', (event) => {
+            event.preventDefault()
+
+            const formData = new FormData(ui.elements.form)
+            const name = formData.get('name')
+            
+            if (name) {
+                this.playerName = name
+            }
+
+            console.log(this.playerName)
+            ui.closeModal()
+        })
+    }
 }
+            
